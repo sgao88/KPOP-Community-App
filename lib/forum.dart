90899b582f743fc638913206982d426791930b8f
r-tafete <60676888+r-tafete@users.noreply.github.com>
2020-10-18 00:24:07 -0400

    Forum Page -- Scroll comments functional

--- lib/forum.dart
+++ lib/forum.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:faker/faker.dart';
+
 
 class ForumPage extends StatelessWidget {
   final Color color;
@@ -7,8 +9,50 @@ class ForumPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: color,
+    return MaterialApp(
+      home: RandomSentence(),
     );
   }
+}
+
+class RandomSentence extends StatefulWidget {
+  @override
+  _RandomSentenceState createState() => _RandomSentenceState();
+}
+
+class _RandomSentenceState extends State<RandomSentence> {
+
+  final _suggestions = <String>[];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _buildSuggestions(),
+    );
+  }
+
+  Widget _buildRow(String sentence) {
+    return ListTile(
+      title: Text(sentence)
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: EdgeInsets.all(16.0),
+        itemBuilder: (context, i) {
+          if (i.isOdd) {
+            return Divider();
+          }
+
+          final index = i ~/ 2;
+          if (index >= -1) {
+            _suggestions.addAll(faker.lorem.sentences(10));
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+
+
 }
